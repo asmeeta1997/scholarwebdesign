80e020a9a35f58e1c61d06d7ee55100fa2277be6
Asmita Karki <asmita.karki@ekbana.info>
2021-02-03 10:42:59 +0545

    chage

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ChangePassword from './ChangePassword';
 const SideBar = () => {
     return (
         <>
             <div className="bg-light border-right">
-                    <div className="list-group list-group-flush list-group-sidebar">
-                        <a href="/" className="mt-3 list-group-item list-group-item-action bg-light">Home</a>
-                        <a href="/" className="list-group-item list-group-item-action bg-light">My Articles</a>
-                        <a href="/" className="list-group-item list-group-item-action bg-light">My Message</a>
-                        <a href="/" className="list-group-item list-group-item-action bg-light">Metrics</a>
-                        <a href="/" className="list-group-item list-group-item-action bg-light">Manage Alert</a>
-                        <a href="/" className="list-group-item list-group-item-action bg-light">My Account</a>
-                    </div>
+                <div className="list-group list-group-flush list-group-sidebar">
+                    <a href="/" className="mt-3 list-group-item list-group-item-action bg-light">Home</a>
+                    <a href="/" className="list-group-item list-group-item-action bg-light">My Articles</a>
+                    <a href="/" className="list-group-item list-group-item-action bg-light">My Message</a>
+                    <a href="/" className="list-group-item list-group-item-action bg-light">Metrics</a>
+                    <a href="/" className="list-group-item list-group-item-action bg-light">Manage Alert</a>
+                    <a href="/" className="list-group-item list-group-item-action bg-light">My Account</a>
                 </div>
+            </div>
         </>
     );
 }
